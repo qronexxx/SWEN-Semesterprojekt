f362a643a649bcd66a9d64d0a6c1cee526863609
Elias <elias.zessner@gmail.com>
2024-10-04 13:53:11 +0200

    init commit

---
+++ src/main/java/at/technikum_wien/Main.java
@@ -0,0 +1,7 @@
+package at.technikum_wien;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/at/technikum_wien/Modles/User.java
@@ -0,0 +1,9 @@
+package at.technikum_wien.Modles;
+
+import lombok.Getter;
+
+public class User {
+    private String username;
+
+
+}
